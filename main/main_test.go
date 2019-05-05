1e09e5a1e3631d40e00553e0608abc587d0316e4
Oscar Martin <oscar.martin@tupl.io>
2019-05-05 22:01:23 +0200

    Fixing UT

--- main/main_test.go
+++ main/main_test.go
@@ -103,7 +103,7 @@ func TestTailOnSingleFile(t *testing.T) {
 
 // Write into a log file. The output should see what is written with a tag
 func TestTailOnSingleFileWithTag(t *testing.T) {
-	path := "./file1.log"
+	path := "./file2.log"
 	tmpfile, closeFunc := createFile(path)
 
 	sendInterruptToMyselfAfter(200 * time.Millisecond)
@@ -119,7 +119,7 @@ func TestTailOnSingleFileWithTag(t *testing.T) {
 
 	defer closeFunc()
 
-	wanted := "[aTag] [file1.log] temporary file's content\n"
+	wanted := "[aTag] [file2.log] temporary file's content\n"
 	if outWriter.String() != wanted {
 		t.Errorf("Found %s; wanted %s", outWriter.String(), wanted)
 	}
@@ -128,10 +128,10 @@ func TestTailOnSingleFileWithTag(t *testing.T) {
 // Write into a txt file and a log file. The output should only see what is
 // written into the log file. Filter based on glob pattern
 func TestTailOnSingleFileWithGlobFilterExecution(t *testing.T) {
-	path := "./file1.log"
+	path := "./file3.log"
 	tmpfile, closeFunc1 := createFile(path)
 
-	pathTxt := "./file1.txt"
+	pathTxt := "./file3.txt"
 	tmpfileTxt, closeFunc2 := createFile(pathTxt)
 
 	sendInterruptToMyselfAfter(200 * time.Millisecond)
@@ -149,7 +149,7 @@ func TestTailOnSingleFileWithGlobFilterExecution(t *testing.T) {
 	defer closeFunc1()
 	defer closeFunc2()
 
-	wanted := "[file1.log] temporary file's content\n"
+	wanted := "[file3.log] temporary file's content\n"
 	if outWriter.String() != wanted {
 		t.Errorf("Found %s; wanted %s", outWriter.String(), wanted)
 	}
@@ -159,10 +159,10 @@ func TestTailOnSingleFileWithGlobFilterExecution(t *testing.T) {
 // written into the log file from the non-nested folder. Filter based on glob pattern
 func TestTailOnNonRecursiveSingleFileWithGlobFilterExecution(t *testing.T) {
 	os.MkdirAll("./tail_folder_test", os.ModePerm)
-	path := "./file1.log"
+	path := "./file4.log"
 	tmpfile, closeFunc1 := createFile(path)
 
-	pathInnerFolder := "./tail_folder_test/file1.log"
+	pathInnerFolder := "./tail_folder_test/file4.log"
 	tmpfileInner, closeFunc2 := createFile(pathInnerFolder)
 
 	sendInterruptToMyselfAfter(200 * time.Millisecond)
@@ -180,7 +180,7 @@ func TestTailOnNonRecursiveSingleFileWithGlobFilterExecution(t *testing.T) {
 	defer closeFunc1()
 	defer closeFunc2()
 
-	if outWriter.String() != "[file1.log] temporary file's content\n" {
+	if outWriter.String() != "[file4.log] temporary file's content\n" {
 		t.Fail()
 	}
 }
@@ -190,10 +190,10 @@ func TestTailOnNonRecursiveSingleFileWithGlobFilterExecution(t *testing.T) {
 func TestTailOnRecursiveSingleFileWithGlobFilterExecution(t *testing.T) {
 	folderName := "./tail_folder_test"
 	os.MkdirAll(folderName, os.ModePerm)
-	path := "./file1.log"
+	path := "./file5.log"
 	tmpfile, closeFunc1 := createFile(path)
 
-	pathInnerFolder := fmt.Sprintf("%s/file1.log", folderName)
+	pathInnerFolder := fmt.Sprintf("%s/file5.log", folderName)
 	tmpfileInner, closeFunc2 := createFile(pathInnerFolder)
 
 	sendInterruptToMyselfAfter(300 * time.Millisecond)
@@ -213,18 +213,19 @@ func TestTailOnRecursiveSingleFileWithGlobFilterExecution(t *testing.T) {
 	defer closeFunc2()
 	defer os.RemoveAll(folderName)
 
-	if outWriter.String() != "[file1.log] temporary file's content\n[tail_folder_test/file1.log] temporary file's content\n" {
-		t.Fail()
+	wanted := "[file5.log] temporary file's content\n[tail_folder_test/file5.log] temporary file's content\n"
+	if outWriter.String() != wanted {
+		t.Errorf("Found: %s; wanted: %s", outWriter.String(), wanted)
 	}
 }
 
 // Write into a txt file and a log file. The output should only see what is
 // written into the log file. Filter based on regex pattern
 func TestTailOnSingleFileWithRegexFilterExecution(t *testing.T) {
-	path := "./file1.log"
+	path := "./file6.log"
 	tmpfile, closeFunc1 := createFile(path)
 
-	pathTxt := "./file1.txt"
+	pathTxt := "./file6.txt"
 	tmpfileTxt, closeFunc2 := createFile(pathTxt)
 
 	sendInterruptToMyselfAfter(200 * time.Millisecond)
@@ -242,7 +243,7 @@ func TestTailOnSingleFileWithRegexFilterExecution(t *testing.T) {
 	defer closeFunc1()
 	defer closeFunc2()
 
-	if outWriter.String() != "[file1.log] temporary file's content\n" {
+	if outWriter.String() != "[file6.log] temporary file's content\n" {
 		t.Fail()
 	}
 }
@@ -250,17 +251,17 @@ func TestTailOnSingleFileWithRegexFilterExecution(t *testing.T) {
 // Write into a txt file and a log file. The output should only see what is
 // written into the log file. Filter based on glob pattern
 func TestTailOnTwoFiles(t *testing.T) {
-	path := "./file1.log"
+	path := "./file7.log"
 	tmpfile, closeFunc1 := createFile(path)
 
-	pathTxt := "./file1.txt"
+	pathTxt := "./file7.txt"
 	tmpfileTxt, closeFunc2 := createFile(pathTxt)
 
 	sendInterruptToMyselfAfter(300 * time.Millisecond)
 
 	outWriter := tail.MakeOutStringWriter()
 	exit := runMain(func() {
-		run(".", "glob", "file1.*", "", false, make([]string, 0), outWriter)
+		run(".", "glob", "file7.*", "", false, make([]string, 0), outWriter)
 	})
 
 	writeInFile(tmpfile, "temporary file's content\n")
@@ -272,7 +273,7 @@ func TestTailOnTwoFiles(t *testing.T) {
 	defer closeFunc1()
 	defer closeFunc2()
 
-	wanted := "[file1.log] temporary file's content\n[file1.txt] temporary file's content\n"
+	wanted := "[file7.log] temporary file's content\n[file7.txt] temporary file's content\n"
 	if outWriter.String() != wanted {
 		t.Errorf("Found: %s; wanted: %s", outWriter.String(), wanted)
 	}
